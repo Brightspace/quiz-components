e1758f170f9d658af422b237dbf02a3c6e4f34e8
Joseph Wong <joseph.wong@d2l.com>
2021-03-24 18:24:49 -0400

    revert changes to question points

--- src/components/d2l-activity-question-points.js
+++ src/components/d2l-activity-question-points.js
@@ -4,20 +4,20 @@ import '@brightspace-ui/core/components/list/list-item.js';
 import '@brightspace-ui/core/components/list/list-item-content.js';
 import '@brightspace-ui/core/components/inputs/input-number.js';
 import '@brightspace-ui/core/components/colors/colors.js';
-import '@brightspace-hmc/foundation-components/components/activity/name/d2l-activity-name';
-import '@brightspace-hmc/foundation-components/components/activity/type/d2l-activity-type';
 
-import { css, LitElement } from 'lit-element/lit-element.js';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { heading4Styles, labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
 import { BaseMixin } from '../mixins/base-mixin';
-import { html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
-import { HypermediaStateMixin } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin';
+import { QuizServiceFactory } from '../services/quizServiceFactory';
 
-class ActivityQuestionPoints extends HypermediaStateMixin(BaseMixin(LitElement)) {
+class ActivityQuestionPoints extends BaseMixin(LitElement) {
 	static get properties() {
 		return {
 			updateDisabled: {
 				type: Boolean
+			},
+			questions: {
+				type: Array
 			}
 		};
 	}
@@ -45,7 +45,9 @@ class ActivityQuestionPoints extends HypermediaStateMixin(BaseMixin(LitElement))
 				display: flex;
 				align-items: baseline;
 			}
-
+			.points_input__label {
+				margin: 12px;
+			}
 			.button_group {
 				margin: 30px;
 			}
@@ -66,21 +68,15 @@ class ActivityQuestionPoints extends HypermediaStateMixin(BaseMixin(LitElement))
 
 	constructor() {
 		super();
-		this._items = [];
+		this.updateDisabled = false;
+
+		this.quizService = QuizServiceFactory.getQuizService();
 	}
 
-	_onActivityUsageCollectionChanged(collection) {
-		this._items = [];
-		collection.onItemsChange((item, index) => {
-			item.onActivityUsageChange((usage) => {
-				this._onActivityCollectionItemChanged(item, usage, index);
-			});
-		});
-	}
+	async connectedCallback() {
+		super.connectedCallback();
 
-	_onActivityCollectionItemChanged(collectionItem, activityUsage, index) {
-		this._items[index] = { collectionItem, activityUsage };
-		this.requestUpdate();
+		this.questions = await this.quizService.getQuestions();
 	}
 
 	_validation() {
@@ -112,6 +108,36 @@ class ActivityQuestionPoints extends HypermediaStateMixin(BaseMixin(LitElement))
 		console.log('Cancelling update');
 	}
 
+	_renderQuestion(question) {
+		return html`
+		<d2l-list-item>
+			<d2l-list-item-content>
+				<div>
+					${ question.title }
+				</div>
+				<div slot="secondary">
+					${ question.secondary }
+				</div>
+			</d2l-list-item-content>
+			<div class="activity_list__points_input" slot="actions">
+				<label for="points_input_${question.id}" class="points_input__label d2l-label-text">
+					${this.localize('inputLabelPoints')}
+				</label>
+				<d2l-input-number
+					id="points_input_${question.id}"
+					label=${this.localize('inputLabelPoints')}
+					value=${ question.points }
+					@change=${this._validation}
+					min=0
+					min-exclusive
+					required
+					label-hidden>
+				</d2l-input-number>
+			</div>
+		</d2l-list-item>
+		`;
+	}
+
 	render() {
 		return html`
 			<div class="main_body">
@@ -125,11 +151,10 @@ class ActivityQuestionPoints extends HypermediaStateMixin(BaseMixin(LitElement))
 				</div>
 				<div class="main_body__activity_list">
 					<d2l-list separators="between">
-						${ this._items && this._items.map(usage => this._renderUsage(usage)) }
+						${ this.questions?.map(question => this._renderQuestion(question)) }
 					</d2l-list>
 				</div>
 			</div>
-
 			<div class="button_group">
 				<d2l-button
 					class="button_group__button"
@@ -146,7 +171,6 @@ class ActivityQuestionPoints extends HypermediaStateMixin(BaseMixin(LitElement))
 			</div>
 		`;
 	}
-
 }
 
 customElements.define('d2l-activity-question-points', ActivityQuestionPoints);
