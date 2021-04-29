75212b6e577916371e50760bdf99bc0a0ea3aa06
Joseph Wong <joseph.wong@d2l.com>
2021-04-29 11:08:58 -0400

    Apply suggestions from code review
    Co-authored-by: louiscoste <66270369+louiscoste@users.noreply.github.com>

--- locales/tr-TR.js
+++ locales/tr-TR.js
@@ -2,7 +2,7 @@ export const val = {
 	'buttonCancel': 'İptal',
 	'buttonUpdate': 'Güncelle',
 	'inputLabelPoints': 'Puanlar',
-	'mainBodyDescription': 'Buradaki soruların detayları güncellendiğinde Brightspace'teki sınava uygulanır ancak Harici Eğitim aracında oluşturulan asıl soruyu etkilemez.',
+	'mainBodyDescription': 'Buradaki soruların detayları güncellendiğinde Brightspace\'teki sınava uygulanır ancak Harici Eğitim aracında oluşturulan asıl soruyu etkilemez.',
 	'mainBodyWarning': 'Not: Harici Öğrenim Aracı soruları içeren tüm Brightspace sınavlarında katılımcılara sayfa başına bir soru görüntülenir.',
 	'mainBodyTitle': 'Soruların Puanlarını Güncelle'
 };
