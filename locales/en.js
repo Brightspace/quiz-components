1f91ebc6232349467cb68ba9d8449ce85ce758a2
Surekha Rao <surekha.rao@d2l.com>
2021-06-23 21:20:30 -0400

    Update lang term value

--- locales/en.js
+++ locales/en.js
@@ -3,6 +3,6 @@ export const val = {
 	'buttonUpdate': 'Update',
 	'inputLabelPoints': 'Points',
 	'mainBodyDescription': 'Updating details to the questions here will be applied to the quiz in Brightspace but won\'t affect the original question created in the External Learning tool.',
-	'mainBodyWarning': 'Note: Any Brightspace quiz with External Learning Tool questions will be displayed to learners as one question per page.',
+	'mainBodyWarning': 'Note: Any Brightspace quiz with External Activities will be displayed to learners as one question per page.',
 	'mainBodyTitle': 'Update Question Points'
 };
