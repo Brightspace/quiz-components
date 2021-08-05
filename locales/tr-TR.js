a70c8c4f2df33c066d15d495327e969e7a02b594
Joseph Wong <joseph.wong@d2l.com>
2021-08-05 10:15:42 -0400

    Update locales/tr-TR.js

--- locales/tr-TR.js
+++ locales/tr-TR.js
@@ -3,7 +3,7 @@ export const val = {
 	'buttonUpdate': 'Güncelle',
 	'externalActivity': 'Harici Etkinlik',
 	'inputLabelPoints': 'Puanlar',
-	'mainBodyDescription': 'Buradaki soruların detayları güncellendiğinde Brightspace'teki sınava uygulanır ancak Harici Eğitim aracında oluşturulan asıl soruyu etkilemez.',
+	'mainBodyDescription': 'Buradaki soruların detayları güncellendiğinde Brightspace\'teki sınava uygulanır ancak Harici Eğitim aracında oluşturulan asıl soruyu etkilemez.',
 	'mainBodyWarning': 'Not: Harici Etkinlikler içeren tüm Brightspace sınavlarında katılımcılara sayfa başına bir soru görüntülenir.',
 	'mainBodyTitle': 'Soru Puanlarını Güncelle'
 };
